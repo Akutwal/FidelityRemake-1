0fce14f95e91bfcf82b649ca5bdfc15a628b1fee
Abhishek <abhishekkutwal34@gmail.com>
2025-04-05 21:21:01 -0400

    Implement structural updates and optimizations across multiple modules

--- src/routes/WhyFidelity.js
+++ src/routes/WhyFidelity.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './WhyFidelity.css';
+import whyFidelityData from '../api/whyFidelityAPI';
+import fidelityLogo from '../images/image 7.png'; // Adjust the path to your logo image
 
 function WhyFidelity() {
-        return (
-            <div>
-                <h1>Why Fidelity</h1>
-                <p>Discover the benefits of choosing Fidelity for your financial needs.</p>
-            </div>
-        );
+  const { title, content } = whyFidelityData;
 
+  return (
+    <div className="why-container">
+      <h1 className="why-title">
+        <span className="why-title-black">We are</span>{' '}
+        <img src={fidelityLogo} alt="Fidelity Logo" className="why-title-logo" />
+      </h1>
+      <div className="why-box">
+        <p>{content[0]}</p>
+        <h2 className="why-subtitle">{content[1]}</h2>
+        <p>{content[2]}</p>
+        <p>{content[3]}</p>
+        <p>{content[4]}</p>
+      </div>
+    </div>
+  );
 }
 
 export default WhyFidelity;
